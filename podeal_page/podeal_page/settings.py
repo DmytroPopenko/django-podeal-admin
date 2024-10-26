b98e06ea421e213cea8abe41da52cf77c0a13459
jirafchick <oleksandr.prudyvus@valthsolutions.com>
2024-10-26 22:30:28 +0300

    feat(podeal-page): update ALLOWED_HOSTS to allow all hosts (main)

--- podeal_page/podeal_page/settings.py
+++ podeal_page/podeal_page/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-um1^2el*@fm(7=q(gpjewg9vxp@+ymrjer5817lnp*)30m*4_w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
