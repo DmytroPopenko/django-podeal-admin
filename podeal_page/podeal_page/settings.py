ac4e1238bf8bf08d45d1a2c699ea556aadffde78
jirafchick <oleksandr.prudyvus@valthsolutions.com>
2024-10-30 22:39:59 +0200

    chore(settings): add CORS middleware and configuration
    - Added `corsheaders` to the installed apps
    - Added `CorsMiddleware` to the middleware stack
    - Set `CORS_ALLOW_ALL_ORIGINS` to `True` to allow all origins to access the API

--- podeal_page/podeal_page/settings.py
+++ podeal_page/podeal_page/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,3 +126,7 @@ STATIC_URL = 'staticfiles/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# CORS
+CORS_ALLOW_ALL_ORIGINS = True
